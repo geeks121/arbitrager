3f504b7f5ce6f1a7e42ed32d693d4160fbbbb16d
yuitoku <exrecord160@gmail.com>
2018-01-23 17:59:59 +0900

    First commit

---
+++ bitflyer_get_board.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+uri = URI.parse("https://api.bitflyer.jp")
+uri.path = '/v1/getboard'
+uri.query = 'product_code=BTC_JPY'
+
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+response = https.get uri.request_uri
+result = JSON.parse(response.body)
+
+puts result["mid_price"]
